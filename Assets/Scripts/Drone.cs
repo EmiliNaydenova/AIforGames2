844f8000408ac4ab5af0d723c5cee6cd0ac8ef79
Emili Naydenova <emili.naydenova@ru.nl>
2025-09-11 11:55:23 +1000

    middle of Step 3 from workshop 7

--- Assets/Scripts/Drone.cs
+++ Assets/Scripts/Drone.cs
@@ -1,7 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Drone : Enemy {
+public class Drone : Enemy
+{
+
+    private float scoutTimer;
+    private float detectTimer;
+    private float scoutTime = 10.0f;
+    private float detectTime = 5.0f;
+    private float detectionRadius = 400.0f;
+    private int newResourceVal;
+    public GameObject newResourceObject;
+    public GameObject motherShip;
+    public Vector3 scoutPosition;
+    public enum DroneBehaviours
+    {
+        Idle,
+        Scouting,
+        Foraging
+    }
+
+    public DroneBehaviours droneBehaviour;
 
     GameManager gameManager;
 
@@ -25,28 +44,39 @@ public class Drone : Enemy {
 
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
 
         gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
 
         rb = GetComponent<Rigidbody>();
+
+        motherShip = gameManager.alienMothership;
+        scoutPosition = motherShip.transform.position;
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
 
         //Acquire player if spawned in
         if (gameManager.gameStarted)
             target = gameManager.playerDreadnaught;
 
         //Move towards valid targets
-        if(target)
+        if (target)
             MoveTowardsTarget();
 
         //Boid cohesion/segregation
         BoidBehaviour();
+        switch (droneBehaviour)
+        {
+            case DroneBehaviours.Scouting:
+                Scouting();
+                break;
+        }
         boidIndex++;
-        if(boidIndex >= gameManager.enemyList.Length)
+        if (boidIndex >= gameManager.enemyList.Length)
         {
             Vector3 cohesiveForce = (cohesionStrength / Vector3.Distance(cohesionPos, transform.position)) * (cohesionPos - transform.position);
             rb.AddForce(cohesiveForce);
@@ -57,22 +87,29 @@ public class Drone : Enemy {
         Quaternion rot = gameManager.enemyList[boidIndex].transform.rotation;
         float dist = Vector3.Distance(transform.position, pos);
 
-        if(dist>0f){
-            if(dist <= separationDistance){
-                float scale = separationStrength/dist;
-                rb.AddForce(scale*Vector3.Normalize(transform.position - pos));
+        if (dist > 0f)
+        {
+            if (dist <= separationDistance)
+            {
+                float scale = separationStrength / dist;
+                rb.AddForce(scale * Vector3.Normalize(transform.position - pos));
             }
-            else if(dist < cohesionDistance && dist > separationDistance){
-                cohesionPos = cohesionPos + pos*(1f/(float)gameManager.enemyList.Length);
+            else if (dist < cohesionDistance && dist > separationDistance)
+            {
+                cohesionPos = cohesionPos + pos * (1f / (float)gameManager.enemyList.Length);
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, 1f);
             }
         }
 
+        
+
     }
 
-    private void MoveTowardsTarget() {
+    private void MoveTowardsTarget()
+    {
         //Rotate and move towards target if out of range
-        if (Vector3.Distance(target.transform.position, transform.position) > targetRadius) {
+        if (Vector3.Distance(target.transform.position, transform.position) > targetRadius)
+        {
 
             //Lerp Towards target
             targetRotation = Quaternion.LookRotation(target.transform.position - transform.position);
@@ -85,6 +122,21 @@ public class Drone : Enemy {
 
     private void BoidBehaviour()
     {
+    }
+
+    private void Scouting()
+    {
+        if (!newResourceObject)
+        {
+            if (Vector3.Distance(transform.position, scoutPosition) < detectionRadius && Time.time > scoutTimer)
+            {
+                
+            }
+        }
+        else
+        {
+
+        } 
     }
 
 }
--- Assets/Scripts/Mothership.cs
+++ Assets/Scripts/Mothership.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Mothership : MonoBehaviour {
 
@@ -8,10 +9,15 @@ public class Mothership : MonoBehaviour {
 
     public GameObject spawnLocation;
 
+    public List<GameObject> drones = new List<GameObject>();
+    public List<GameObject> scouts = new List<GameObject>();
+
+    public int maxScouts = 4;
     // initialise the boids
     void Start() {
 
-        for (int i = 0; i < numberOfEnemies; i++) {
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
 
             Vector3 spawnPosition = spawnLocation.transform.position;
 
@@ -19,12 +25,28 @@ public class Mothership : MonoBehaviour {
             spawnPosition.y = spawnPosition.y + Random.Range(-50, 50);
             spawnPosition.z = spawnPosition.z + Random.Range(-50, 50);
 
-            Instantiate(enemy, spawnPosition, spawnLocation.transform.rotation);
+            //Instantiate(enemy, spawnPosition, spawnLocation.transform.rotation);
+
+            GameObject thisEnemy = Instantiate(enemy, spawnPosition, spawnLocation.transform.rotation) as GameObject;
+            drones.Add(thisEnemy);
         }
     }
 
+    //Introducing a timer
+    public float scoutSpawnInterval = 2f;
+    private float scoutSpawnTimer = 0f;
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
+        scoutSpawnTimer += Time.deltaTime;
+        if (scouts.Count < maxScouts && scoutSpawnTimer >= scoutSpawnInterval)
+        {
+            scoutSpawnTimer = 0f;
+            scouts.Add(drones[0]);
+            drones.Remove(drones[0]);
+
+            scouts[scouts.Count - 1].GetComponent<Drone>().droneBehaviour = Drone.DroneBehaviours.Scouting;
+        }
 
     }
 }
